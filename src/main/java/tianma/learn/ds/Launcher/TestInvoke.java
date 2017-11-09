ead5c17ea856506a626143e5b63e607ffef76b39
rputho2 <rputho2@uic.edu>
2017-11-09 01:21:00 -0600

    Fixing a few things in folder and making inner classes public to run the Reflection

--- src/main/java/tianma/learn/ds/Launcher/TestInvoke.java
+++ src/main/java/tianma/learn/ds/Launcher/TestInvoke.java
@@ -1,29 +1,30 @@
-package tianma.learn.ds;
+package tianma.learn.ds.Launcher;
 
 import java.lang.reflect.Method;
 
-        import javassist.ClassPool;
-        import javassist.CtClass;
-        import javassist.*;
-        import java.lang.reflect.InvocationTargetException;
-        import java.lang.reflect.Method;
-        import java.util.Scanner;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.*;
+import tianma.learn.ds.Launcher.Config;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Scanner;
 
 public class TestInvoke {
 
     // test Javassist
-    public static void main (String[] args) throws NotFoundException, CannotCompileException, InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+    public static void main(String[] args) throws NotFoundException, CannotCompileException, InvocationTargetException, IllegalAccessException, NoSuchMethodException {
         /*
            tianma.learn.ds.sort.BubbleSorter2 currentClass = new tianma.learn.ds.sort.BubbleSorter2();
         Method methodCall =  currentClass.getClass().getDeclaredMethod("sort",int[].class);
         // int[] a = {9, 3, 2, 6, 8, 7, 1, 12, 4, 11};
         methodCall.invoke(currentClass,Config.BubbleSorter.input_array);
          */
-        tianma.learn.ds.string.main.StringMatchSample2.ViolentStringMatcher currentClass = new tianma.learn.ds.string.main.StringMatchSample2.ViolentStringMatcher();
-        Method methodCall =  currentClass.getClass().getDeclaredMethod("indexOf",String.class,String.class);
+        tianma.learn.ds.string.main.StringMatchSample_1.ViolentStringMatcher currentClass = new tianma.learn.ds.string.main.StringMatchSample_1.ViolentStringMatcher();
+        Method methodCall = currentClass.getClass().getDeclaredMethod("indexOf", String.class, String.class);
         // int[] a = {9, 3, 2, 6, 8, 7, 1, 12, 4, 11};
-        methodCall.invoke(currentClass,Config.StringMatchSample.source,Config.StringMatchSample.pattern);
-
+        methodCall.invoke(currentClass, Config.StringMatchSample.source, Config.StringMatchSample.pattern);
 
 
     }
